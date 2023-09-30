a844e22caad9fc228cc5e7c4860c8c86a919d441
Norbiox <norbertchmiel.it@gmail.com>
2023-09-30 09:59:11 +0200

    do not create instance directory

--- lyrics_chordifier/__init__.py
+++ lyrics_chordifier/__init__.py
@@ -26,12 +26,6 @@ def create_app(test_config=None):
         # load the test config if passed in
         app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
     app.register_blueprint(api_blueprint)
     app.register_blueprint(views_blueprint)
 
