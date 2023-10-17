09478a268b0f8ccdeafe3bb851fdf19538580014
Norbiox <norbertchmiel.it@gmail.com>
2023-10-17 22:36:59 +0200

    add admin user

--- app/auth.py
+++ app/auth.py
@@ -1,3 +1,9 @@
+"""Applications authentication module
+
+Authentication system is based on HTTPBasicAuth and consists of 2 users:
+- admin
+- regular user.
+"""
 import os
 
 from flask_httpauth import HTTPBasicAuth
@@ -5,14 +11,33 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = HTTPBasicAuth()
 
+admin_credentials = (
+    os.getenv("HTTP_AUTH_ADMIN_USER"),
+    generate_password_hash(os.getenv("HTTP_AUTH_ADMIN_PASSWORD", "")),
+)
 credentials = (
     os.getenv("HTTP_AUTH_USER", ""),
     generate_password_hash(os.getenv("HTTP_AUTH_PASSWORD", "")),
 )
 
+if admin_credentials[0] == credentials[0]:
+    raise ValueError("admin username cannot be identical as user username")
+
+
+def is_admin(username: str) -> bool:
+    """Verify if given username is admin"""
+    if not admin_credentials[0]:
+        return False
+
+    return username == admin_credentials[0]
+
 
 @auth.verify_password
 def verify_credentials(username: str, password: str) -> str | None:
+    """Validates given credentials"""
+    if is_admin(username) and check_password_hash(admin_credentials[1], password):
+        return username
+
     if not credentials[0]:
         return username
 
